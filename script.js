71b094796344e48777945ac67f4ecb88b60b9f57
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-09-12 14:42:47 +0300

    Fixed the poppup

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ const displayList = data => {
         <td>${person.jobArea}</td>
         <td>${person.phone}</td>
         <td>
-            <button class="edit">
+            <button class="edit" id="${person.id}">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="pencil w-6 h-6"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
             </button>
-            <button class="delete">
+            <button class="delete" id="${person.id}">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="trash w-6 h-6"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
             </button>
             </td>
@@ -43,35 +43,50 @@ const displayList = data => {
 
 
 
-const editPartner = (options) => {
+// const editPartner = e => {
+//   const button = e.target.closest()
+//   const showAs = editPartnerPopup(person);
+
+//   if(showAs) {
+//      displayList(persons);
+//   }
+	
+// 	// code edit function here
 
 	
-	// code edit function here
 
-	return new Promise (async function(resolve) {
-   const popup = document.createElement('form');
-   const relate = persons.find(person => person.id === persons.id);
-   popup.classList.add('.popup');
-   popup.insertAdjacentHTML('afterbegin', `
+
+
+// };
+
+
+const editPartnerPopup = (id) => {
+  const relate = persons.find(person => person.id === id);
+  console.log(relate);
+
+    const popup = document.createElement('form');
+    console.log(relate);
+    popup.classList.add('.popup');
+    popup.insertAdjacentHTML('afterbegin', `
 	 <fieldset style="border: none;">
       <label for="namelast">LastName</label><br>
-      <input type="text" value="${resolve.lastName}" id="namelast">
+      <input type="text" value="${relate.lastName}" id="namelast">
     </fieldset>
     <fieldset style="border: none;">
       <label for="">FisrtName</label><br>
-      <input type="text" value="${resolve.firstName}" id="">
+      <input type="text" value="${relate.firstName}" id="firstNam">
     </fieldset style="border: none;">
     <fieldset style="border: none;">
       <label for="">JobTitle</label><br>
-      <input type="text" value="${resolve.jobTitle}" id="">
+      <input type="text" value="${relate.jobTitle}" id="">
     </fieldset>
     <fieldset style="border: none;">
       <label for="">JobArea</label><br>
-      <input type="text" value="${resolve.jobArea}" id="">
+      <input type="text" value="${relate.jobArea}" id="">
     </fieldset>
     <fieldset style="border: none;">
       <label for="">Phone number</label><br>
-      <input type="text" value="${resolve.phoneNumber}" id="">
+      <input type="text" value="${relate.phoneNumber}" id="">
     </fieldset>
     <div class="button-sub">
       <button class="button__save">Save</button>
@@ -80,26 +95,17 @@ const editPartner = (options) => {
    `);
 
     document.body.appendChild(popup);
-    
-	
-  });
 
 
-
-  popup.classList.add('open');
-
+    popup.classList.add('open');
 
 
+  
+
+  // create edit popup here
 };
 
 
-
-const listenTothepencil = (e) => {
-  const button = e.target.closest('.edit');
-     if(button) {
-       editPartner()
-     }
-}
 
 
 
@@ -109,18 +115,26 @@ const lists = [
 ]
 
 
-const editPartnerPopup = () => {
-	// create edit popup here
+
+
+const deletePartner = (persons) => {
+
+  const wantdelete = persons.find(person => person.id === persons.id);
+  const deletesure = deleteDeletePopup(persons);
+
+ 
+
+
+	// code delete function gere
 };
 
-const deletePartner = () => {
+const deleteDeletePopup = () => {
 
   const popup = document.createElement('article');
-  const wantdelete = persons.find(person => person.id === persons.id);
   popup.classList.add('.delete_confirm');
   popup.insertAdjacentHTML('afterbegin', `
   <p class="deleteparagraph">
-      Are you sure you want to delete this partener
+      Are you sure you want to delete this partner
     </p>
     <div class="container__buttom">
       <button class="confirm_buttom yes__sure"> Yes </button>
@@ -132,11 +146,6 @@ const deletePartner = () => {
 
   popup.classList.add('.delete_confirm');
 
-
-	// code delete function gere
-};
-
-const deleteDeletePopup = () => {
 	// create confirmation popup here
 };
 
@@ -157,5 +166,26 @@ async function show() {
 
 displayList(persons);
 
-tbody.addEventListener('click', listenTothepencil);
-tbody.addEventListener('click', deletePartner);
+const handleClick = e => {
+ const editButton = e.target.closest('.edit');
+ const deletButton = e.target.closest('.delete');
+
+ if(editButton) {
+  const id = editButton.id; 
+   editPartnerPopup(id);
+
+ }
+
+
+ if(deletButton) {
+   
+  const id = deletButton.id;
+  deletePartner(id);
+   
+ }
+
+
+}
+
+tbody.addEventListener('click', handleClick);
+
